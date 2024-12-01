1524b10473f297869f93d29e1ffd5983deaaaa07
comels <come.l.s@hotmail.fr>
2024-12-01 20:14:14 +0100

    chemin photo

--- src/components/Prod.jsx
+++ src/components/Prod.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default function Prod() {
   const logos = [
     { src: '/logos/aigle.png', alt: 'Logo Aigle' },
@@ -13,20 +11,17 @@ export default function Prod() {
   return (
     <div className="bg-white py-16">
       <div className="max-w-9xl mx-auto">
-        <p className="mb-10 font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl md:text-center">
+        <p className="mb-10 font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl md:mb-20 md:text-center">
           Nos clients
         </p>
 
         <div className="grid grid-cols-2 gap-12 overflow-hidden sm:rounded-2xl xl:grid-cols-6">
           {logos.map((logo, index) => (
-            <div key={index} className="">
-              <Image
+            <div key={index}>
+              <img
                 alt={logo.alt}
                 src={logo.src}
-                width={158}
-                height={48}
                 className="max-h-7 w-full object-contain"
-                priority
               />
             </div>
           ))}
